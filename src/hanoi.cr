65f6d68fc5d671b5a3b8eda089fdd88e5683cdc7
Akiicat <aaaa1379@gmail.com>
2017-08-04 20:54:36 +0800

    towers of hanoi

---
+++ src/hanoi.cr
@@ -0,0 +1,14 @@
+def hanoi(n, a, b, c)
+  if n == 1
+    [{from: a, to: c}]
+  else
+    hanoi(n - 1, a, c, b) + hanoi(1, a, b, c) + hanoi(n - 1, b, a, c)
+  end
+end
+
+print "input an integer:"
+
+input = (gets || 0).to_i
+hanoi(input, "A", "B", "C").each do |move|
+  puts "from #{move[:from]} to #{move[:to]}"
+end
